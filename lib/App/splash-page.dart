a792bf8a1bb469f47560a0953736f9394cdb6a51
Hellyson-Ferreira <hellysonferreira.hf1@gmail.com>
2020-06-07 19:11:55 -0300

    Created Splash screen

---
+++ lib/App/splash-page.dart
@@ -0,0 +1,55 @@
+import 'package:ecoleta_flutter/App/home-page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+class SplashPage extends StatefulWidget {
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0XFFE5E5E5),
+      body: Stack(
+        children: <Widget>[
+          SplashScreen(
+            seconds: 4,
+            backgroundColor: Colors.white,
+            navigateAfterSeconds: HomePage(),
+            loaderColor: Colors.white,
+          ),
+          Container(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SvgPicture.asset(
+                    'assets/logo.svg',
+                    width: 123.25,
+                    height: 112.75,
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Text(
+                    'Carregando app...',
+                    style: GoogleFonts.roboto(
+                      fontSize: 15,
+                      fontStyle: FontStyle.normal,
+                      color: Color(0xFF6C6C80)
+                      
+                    )
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ecoleta_flutter/App/splash-page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,12 +9,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Color(0xFF34CB79),
+        backgroundColor: Color(0xFFF0F0F5),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomePage(),
+      home: SplashPage(),
     );
   }
 }
