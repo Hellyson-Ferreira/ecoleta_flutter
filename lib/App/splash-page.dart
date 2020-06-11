70759042678356071f4c916d525a99192ae3825d
Hellyson-Ferreira <hellysonferreira.hf1@gmail.com>
2020-06-11 16:41:07 -0300

    Update splash-page.dart

--- lib/App/splash-page.dart
+++ lib/App/splash-page.dart
@@ -1,6 +1,6 @@
-import 'package:ecoleta_flutter/App/home-page.dart';
+import 'package:ecoleta_flutter/App/start-page.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:splashscreen/splashscreen.dart';
 
@@ -13,40 +13,43 @@ class _SplashPageState extends State<SplashPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0XFFE5E5E5),
-      body: Stack(
-        children: <Widget>[
-          SplashScreen(
-            seconds: 4,
-            backgroundColor: Colors.white,
-            navigateAfterSeconds: HomePage(),
-            loaderColor: Colors.white,
-          ),
-          Container(
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  SvgPicture.asset(
-                    'assets/logo.svg',
-                    width: 123.25,
-                    height: 112.75,
-                  ),
-                  SizedBox(
-                    height: 40,
-                  ),
-                  Text(
-                    'Carregando app...',
-                    style: GoogleFonts.roboto(
-                      fontSize: 15,
-                      fontStyle: FontStyle.normal,
-                      color: Color(0xFF6C6C80)
-                      
-                    )
-                  ),
-                ],
+      backgroundColor: Color(0XFFF0F0F5),
+      body: Column(
+        children: [
+          Stack(
+            children: <Widget>[
+              SplashScreen(
+                seconds: 4,
+                backgroundColor: Colors.white,
+                navigateAfterSeconds: StartPage(),
+                loaderColor: Colors.white,
               ),
-            ),
+              Container(
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      SvgPicture.asset(
+                        'assets/logo.svg',
+                        width: 123.25,
+                        height: 112.75,
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      Text(
+                        'Carregando app...',
+                        style: GoogleFonts.roboto(
+                          fontSize: 15,
+                          fontStyle: FontStyle.normal,
+                          color: Color(0xFF6C6C80) 
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
